b6accc74083592d96e17006d63e3d2a565db6253
Lucas <lucasramos1123.lr29@gmail.com>
2023-06-30 21:48:27 -0300

    feat: ensure login, account creation and sign out button are shown based on authentication atate

--- src/Components/header/header.component.tsx
+++ src/Components/header/header.component.tsx
@@ -1,10 +1,14 @@
+//Styles
+import { HeaderContainer, HeaderItems, HeaderItem, HeaderTitle } from './header.styles'
+import { userContext } from '../../contexts/user.context';
+import { useContext } from 'react';
+
+// Utils
 import { BsCart3 } from 'react-icons/bs'
 import { useNavigate } from 'react-router-dom'
 import { signOut } from 'firebase/auth';
 import { auth } from '../../config/firebase.config';
 
-//Styles
-import { HeaderContainer, HeaderItems, HeaderItem, HeaderTitle } from './header.styles'
 
 const Header = () => {
 
@@ -20,15 +24,29 @@ const Header = () => {
         navigate('/sign-up')
     }
 
+    const {isAuthenticated} = useContext(userContext)
+
     return (
         <HeaderContainer>
             <HeaderTitle>CLUB CLOTHING</HeaderTitle>
             
             <HeaderItems>
                 <HeaderItem onClick={handleExplorarLogin}>Explorar</HeaderItem>
-                <HeaderItem onClick={handleLoginClick}>Login</HeaderItem>
-                <HeaderItem onClick={handleSignUpClick}>Criar Conta</HeaderItem>
-                <HeaderItem onClick={() => signOut(auth)}>Sair</HeaderItem>
+                {
+                    !isAuthenticated && (
+                        <>
+                            <HeaderItem onClick={handleLoginClick}>Login</HeaderItem>
+                            <HeaderItem onClick={handleSignUpClick}>Criar Conta</HeaderItem>
+                        </>
+                    )
+                }
+                {
+                    isAuthenticated && (
+                        <>
+                            <HeaderItem onClick={() => signOut(auth)}>Sair</HeaderItem>
+                        </>
+                    )
+                }
                 <HeaderItem>
                     <BsCart3 size={25}/>
                     <p style={{marginLeft: 5}}>5</p>
