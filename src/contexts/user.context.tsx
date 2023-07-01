32ebd2272ce3c744eab5e94dec8e9e77b00fcf43
Lucas <lucasramos1123.lr29@gmail.com>
2023-06-30 21:38:35 -0300

    feat: add userContext

---
+++ src/contexts/user.context.tsx
@@ -0,0 +1,44 @@
+import { FunctionComponent, createContext, useState,  } from "react";
+import User from "../types/user.types";
+
+interface IUserContext {
+    currentUser: User | null,
+    isAuthenticated: boolean,
+    loginUser: (user: User) => void
+    logoutUser: () => void
+}
+interface userContextProps {
+    children: any
+}
+
+export const userContext = createContext<IUserContext>({
+    currentUser: null,
+    isAuthenticated: false,
+    loginUser: () => {},
+    logoutUser: () => {}
+});
+
+const UserContextProvider: FunctionComponent<userContextProps> = ({children}) =>{
+
+    const [currentUser, setCurrentUser] = useState<User | null>(null)
+
+    //se current user for diferente de null, significa que ele esta autenticado
+    const isAuthenticated = currentUser !== null
+
+    const loginUser = (user: User) => {
+        setCurrentUser(user)
+    }
+
+    const logoutUser = () => {
+        setCurrentUser(null)
+    }
+
+
+    return(
+        <userContext.Provider value={{ currentUser, isAuthenticated, loginUser, logoutUser }} >
+            {children}
+        </userContext.Provider>
+    )
+}
+
+export default UserContextProvider;
