7e03382fd7f5bdf5dcdf9f9705017e0ea5b2ecb5
Lucas <lucasramos1123.lr29@gmail.com>
2023-06-30 21:46:20 -0300

    feat: add update userContext on onAuthStageChange

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { onAuthStateChanged } from 'firebase/auth'
-
 // Pages
 import HomePage from './Pages/home/home.page';
 import LoginPage from './Pages/login/login.page';
 import SingUpPage from './Pages/sign-up/sign-up.pages';
-import { auth } from './config/firebase.config';
-import { FunctionComponent } from 'react';
+
+// Utils
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth'
+import { FunctionComponent, useContext } from 'react';
+import { auth, db } from './config/firebase.config';
+import { userContext } from './contexts/user.context'
+import { collection, getDocs, query, where } from 'firebase/firestore';
 
 const App: FunctionComponent = () => {
+   
+  const { currentUser, isAuthenticated, loginUser, logoutUser } = useContext(userContext)
 
-  onAuthStateChanged(auth, (user) => {
-    console.log(user)
+  console.log({currentUser})
+  console.log({isAuthenticated})
+  console.log({loginUser})
+
+  //se o usuario estiver logado no contexto, e o usuario do firebase(sign out)
+  //devemos limpar o contexto(sign out)
+  onAuthStateChanged(auth, async (user) => {
+    const isSingnInOut = isAuthenticated && !user
+    if(isSingnInOut){
+      return logoutUser()
+    }
+
+    //se o usuario for nulo no contexto, e não for nulo no firebase
+    //devemos fazer login
+    const isSigninIn = !isAuthenticated && user
+    if(isSigninIn){
+
+      const QuerySnapshot = await getDocs(query(collection(db, 'users'), where('id', '==', user.uid)))
+
+      const userFromFirestore = QuerySnapshot.docs[0]?.data()
+
+      return loginUser(userFromFirestore as any)
+    }
   })
 
   return (
