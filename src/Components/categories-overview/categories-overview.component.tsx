4ba98ec5f490f01ee918585c0e78f7075a79bdfd
Lucas <lucasramos1123.lr29@gmail.com>
2023-07-13 09:49:58 -0300

    feat: add Loading Component to Categories Overview

--- src/Components/categories-overview/categories-overview.component.tsx
+++ src/Components/categories-overview/categories-overview.component.tsx
@@ -2,15 +2,19 @@ import { FunctionComponent, useContext, useEffect } from "react";
 import { Container } from "./categories-overview.styles";
 import { CategoryContext } from "../../contexts/categories.context";
 import CategoryOverview from "../category-overview/category-overview.component";
+import Loading from "../loading/loading.components";
 
 const CategoriesOverview: FunctionComponent = () => {
-    const { categories, fetchCategories } = useContext(CategoryContext)
+    const { categories, isLoading, fetchCategories } = useContext(CategoryContext)
 
     useEffect(() => {
         if (categories.length === 0){
             fetchCategories()
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
+    if(isLoading) return <Loading/>
 
     return ( 
         <>
